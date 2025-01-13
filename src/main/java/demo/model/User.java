49424a6ea826a4d618a68670cb6284a1d78dd60f
herik-erbeth <herik.docarmo@hotmail.com>
2025-01-12 23:13:52 -0300

    feat(model): add User model with name and blog properties

---
+++ src/main/java/demo/model/User.java
@@ -0,0 +1,36 @@
+package demo.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class User {
+
+    private String name;
+    private String blog;
+
+    public User(String name, String blog) {
+        this.name = name;
+        this.blog = blog;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBlog() {
+        return blog;
+    }
+
+    public void setBlog(String blog) {
+        this.blog = blog;
+    }
+
+    @Override
+    public String toString() {
+        return "User [name=" + name + ", blog=" + blog + "]";
+    }
+}
